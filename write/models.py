0717585a5a87328b7c12d7edc314b7913e1aecb6
vivienne <vivienne.2214@yahoo.com>
2023-01-15 18:47:08 +0000

    create admin panel and superuser

--- write/models.py
+++ write/models.py
@@ -1,22 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class User(models.Model):
-    slug = models.SlugField(unique=True)
-    first_name = models.CharField(max_length=256)
-    last_name = models.CharField(max_length=256)
-    username = models.CharField(max_length=256, unique=True)
-    email = models.CharField(max_length=256, unique=True)
-    password = models.CharField(max_length=256)
-    birthday = models.DateTimeField()
-    date_joined = models.DateTimeField(auto_now_add=True)
-    admin = models.BooleanField(default=False)
+# class User(models.Model):
+#     slug = models.SlugField(unique=True)
+#     first_name = models.CharField(max_length=256)
+#     last_name = models.CharField(max_length=256)
+#     username = models.CharField(max_length=256, unique=True)
+#     email = models.CharField(max_length=256, unique=True)
+#     password = models.CharField(max_length=256)       #characters not hidden
+#     birthday = models.DateTimeField()
+#     date_joined = models.DateTimeField(auto_now_add=True)
+#     admin = models.BooleanField(default=False)
 
-    class Meta:
-        ordering = ['date_joined']
+#     class Meta:
+#         ordering = ['-date_joined']
 
-    def __str__(self):
-        return self.username
+#     def __str__(self):
+#         return self.username
 
 
 class Post(models.Model):
@@ -26,8 +27,8 @@ class Post(models.Model):
         POEMS = 'P', 'Poems'
 
     category = models.CharField(max_length=256, choices=Categories.choices)
-    slug = models.SlugField(unique=True)
     title = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='write_post')
     created_date = models.DateTimeField(auto_now_add=True)
@@ -37,14 +38,16 @@ class Post(models.Model):
         User, related_name='writepost_like', blank=True)
 
     class Meta:
-        ordering = ['created_date']
+        ordering = ['-created_date']
 
     def __str__(self):
         return self.title
 
+    def number_of_likes(self):
+        return self.likes.count()
+
 
 class Comment(models.Model):
-    slug = models.SlugField(unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
@@ -52,10 +55,10 @@ class Comment(models.Model):
     approved = models.BooleanField(default=False)
 
     class Meta:
-        ordering = ['created_date']
+        ordering = ['-created_date']
 
     def __str__(self):
-        return f"Comment {self.content} by {self.author}"
+        return f"{self.content} by {self.author} on {self.created_date}"
 
     def number_of_comments(self):
         return self.content.count()
