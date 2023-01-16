c3d52237691726f4cbe42fbb59ab4227340899c3
vivienne <vivienne.2214@yahoo.com>
2023-01-16 14:38:10 +0000

    add amdin approval for Post & Comment sections

--- write/admin.py
+++ write/admin.py
@@ -11,6 +11,10 @@ class PostAdmin(SummernoteModelAdmin):
     search_fields = ['category', 'title', 'content', 'approved']
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content')
+    actions = ['approve_posts']
+
+    def approve_posts(self, request, queryset):
+        queryset.update(approved=True)
 
 
 @admin.register(Comment)
@@ -19,3 +23,7 @@ class CommentAdmin(admin.ModelAdmin):
     list_display = ('name', 'comment', 'created_date', 'approved')
     list_filter = ('created_date', 'approved')
     search_fields = ('name', 'post', 'created_date', 'approved')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
